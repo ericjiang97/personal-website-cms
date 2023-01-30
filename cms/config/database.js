6d2bc40da0a02db3cdb98956348879e51ed19784
Eric Jiang <eric@ericjiang.dev>
2023-01-30 21:32:51 +0800

    enable ssl for postgres connection

--- cms/config/database.js
+++ cms/config/database.js
@@ -9,9 +9,9 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
-      // ssl: {
-      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      // },
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
     debug: false,
   },
