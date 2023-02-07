17859d946015ca575c5c3d71f343084d8c852c02
Eric Jiang <eric@ericjiang.dev>
2023-02-07 17:44:32 +0800

    more work

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,18 +1,28 @@
 const path = require("path");
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
-    },
-    debug: false,
-  },
+  connection:
+    env.NODE_ENV === "PRODUCTION "
+      ? {
+          client: "postgres",
+          connection: {
+            host: env("DATABASE_HOST", "127.0.0.1"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME", "strapi"),
+            user: env("DATABASE_USERNAME", ""),
+            password: env("DATABASE_PASSWORD", ""),
+            ssl: {
+              rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+            },
+          },
+          debug: false,
+        }
+      : {
+          client: "sqlite",
+          connection: {
+            filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+          },
+          useNullAsDefault: true,
+          debug: false,
+        },
 });
